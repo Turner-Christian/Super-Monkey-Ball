bb2a2878e39bfe6a60ef5604e2d23ae98b44e2c5
Turner-Christian <keycoderk@gmail.com>
2025-05-31 13:39:54 -0400

    Camera Movement

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,140 +2,36 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public Camera mainCamera; // Reference to the main camera
+    public static BallController Instance; // Singleton instance for easy access
     public float moveForce = 10f; // Force applied to the ball when moving
-    public float maxZAngle = 35f; // Variable to store the rotation angle
-    public float maxXAngle = 15f; // Variable to store the rotation angle
-    public float maxYOffset = 0.4f; // Maximum Y offset for the camera when moving forward
-    public float cameraSmoothness = 0.5f; // Smoothness factor for camera movement
-    private Rigidbody _rb; // Reference to the ball's Rigidbody component
-    private float _originalXAngle;
-    private float _originalYPosition; // Default Y position of the camera
+    public Rigidbody rb; // Reference to the ball's Rigidbody component
+
+    void Awake()
+    {
+        Instance = this; // Set the singleton instance
+    }
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>(); // Get the Rigidbody component attached to the bal
-        _originalXAngle = mainCamera.transform.eulerAngles.x; // Store the initial X angle of the camera
-        _originalYPosition = mainCamera.transform.position.y; // Store the initial Y position of the camera
-    }
-
-    void Update()
-    {
-        float speed = _rb.linearVelocity.magnitude;
-        Debug.Log("Speed: " + speed);
-        // GOING LEFT
-        // TODO: UGLY ASS CODE, REFACTOR THIS        
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            float input = Input.GetAxis("Horizontal");
-            float targetAngle = Mathf.Clamp(input * maxZAngle, -maxZAngle, maxZAngle); // Clamp the angle to the max angle
-
-            mainCamera.transform.eulerAngles = new Vector3(
-                mainCamera.transform.eulerAngles.x,
-                mainCamera.transform.eulerAngles.y,
-                targetAngle
-            );
-        }
-        // GOING RIGHT
-        // TODO: UGLY ASS CODE, REFACTOR THIS        
-        else if (Input.GetAxis("Horizontal") > 0)
-        {
-            float input = Input.GetAxis("Horizontal");
-            float targetAngle = Mathf.Clamp(input * maxZAngle, -maxZAngle, maxZAngle); // Clamp the angle to the max angle
-
-            mainCamera.transform.eulerAngles = new Vector3(
-                mainCamera.transform.eulerAngles.x,
-                mainCamera.transform.eulerAngles.y,
-                targetAngle
-            );
-        }
-        // NO INPUT HORIZONTAL
-        // TODO: UGLY ASS CODE, REFACTOR THIS        
-        else
-        {
-            mainCamera.transform.eulerAngles = new Vector3(
-                mainCamera.transform.eulerAngles.x,
-                mainCamera.transform.eulerAngles.y,
-                0 // Reset the angle when no input is detected
-            );
-        }
-        // GOING FORWARD
-        // TODO: UGLY ASS CODE, REFACTOR THIS
-        if (Input.GetAxis("Vertical") > 0)
-        {
-            float input = Input.GetAxis("Vertical");
-            float targetAngle = Mathf.Clamp(
-                _originalXAngle - (input * maxXAngle),
-                _originalXAngle - maxXAngle,
-                _originalXAngle
-            );
-            float targetYOffset = Mathf.Clamp(
-                _originalYPosition - ((input / 2f) * _originalYPosition),
-                1.6f,
-                _originalYPosition
-            );
-            // Calculate the Y offset based on input
-            mainCamera.transform.position = new Vector3(
-                mainCamera.transform.position.x,
-                targetYOffset,
-                mainCamera.transform.position.z
-            );
-            mainCamera.transform.eulerAngles = new Vector3(
-                targetAngle,
-                mainCamera.transform.eulerAngles.y,
-                mainCamera.transform.eulerAngles.z
-            );
-        }
-        // GOING BACKWARD
-        // TODO: UGLY ASS CODE, REFACTOR THIS        
-        else if (Input.GetAxis("Vertical") < 0)
-        {
-            float input = -Input.GetAxis("Vertical");
-            float targetAngle = Mathf.Clamp(
-                _originalXAngle + (input * maxXAngle),
-                _originalXAngle,
-                _originalXAngle + maxXAngle
-            );
-            float targetYOffset = Mathf.Clamp(
-                _originalYPosition + ((input / 2f) * _originalYPosition),
-                _originalYPosition,
-                _originalYPosition + .25f
-            );
-            // Calculate the Y offset based on input
-            mainCamera.transform.position = new Vector3(
-                mainCamera.transform.position.x,
-                targetYOffset,
-                mainCamera.transform.position.z
-            );
-            mainCamera.transform.eulerAngles = new Vector3(
-                targetAngle,
-                mainCamera.transform.eulerAngles.y,
-                mainCamera.transform.eulerAngles.z
-            );
-        }
-        // NO INPUT VERTICAL
-        // TODO: UGLY ASS CODE, REFACTOR THIS        
-        else
-        {
-            mainCamera.transform.eulerAngles = new Vector3(
-                _originalXAngle,
-                mainCamera.transform.eulerAngles.y,
-                mainCamera.transform.eulerAngles.z
-            );
-            mainCamera.transform.position = new Vector3(
-                mainCamera.transform.position.x,
-                _originalYPosition,
-                mainCamera.transform.position.z
-            );
-        }
+        rb = GetComponent<Rigidbody>(); // Get the Rigidbody component attached to the bal
     }
 
     void FixedUpdate()
     {
-        float moveX = Input.GetAxis("Horizontal"); // Get horizontal input (A/D or Left/Right arrows)
-        float moveZ = Input.GetAxis("Vertical"); // Get vertical input (W/S or Up/Down arrows)
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
 
-        Vector3 force = new Vector3(moveX, 0, moveZ) * moveForce; // Calculate the force vector based on input
-        _rb.AddForce(force); // Apply the force to the ball's Rigidbody
+        // Get camera forward direction, projected onto XZ plane
+        Vector3 camForward = Camera.main.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        // Get right vector from forward
+        Vector3 camRight = new Vector3(camForward.z, 0, -camForward.x);
+
+        // Combine input with camera direction
+        Vector3 moveDir = (camForward * moveZ + camRight * moveX).normalized;
+
+        rb.AddForce(moveDir * moveForce);
     }
 }
--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,15 +2,51 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float maxZAngle = 15f; // Maximum angle for Z-axis rotation
+    public float smoothSpeed = 15f; // Smoothing speed
+    private float _originalXAngle;
+    private float _originalZAngle;
+    private float _currentXAngle;
+    private float _currentZAngle;
+
     void Start()
     {
-        
+        _originalZAngle = transform.eulerAngles.z;
+        _originalXAngle = transform.eulerAngles.x;
+        _currentXAngle = _originalXAngle;
+        _currentZAngle = _originalZAngle;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        Vector3 rot = transform.eulerAngles;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        float targetZAngle =
+            (horizontalInput != 0)
+                ? Mathf.Clamp(maxZAngle * horizontalInput, -maxZAngle, maxZAngle)
+                : _originalZAngle;
+
+        float targetXAngle = _originalXAngle + (-verticalInput * 5f);
+
+        // Smoothly interpolate angles
+        _currentXAngle = Mathf.LerpAngle(
+            _currentXAngle,
+            targetXAngle,
+            Time.deltaTime * smoothSpeed
+        );
+        _currentZAngle = Mathf.LerpAngle(
+            _currentZAngle,
+            targetZAngle,
+            Time.deltaTime * smoothSpeed
+        );
+
+        transform.eulerAngles = new Vector3(_currentXAngle, rot.y, _currentZAngle);
     }
 }
